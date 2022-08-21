43663447b201bcc5fc21a5a13d3a6d2ff0193849
KCas <78678795+CKodes@users.noreply.github.com>
2022-08-22 06:07:48 +0800

    feat: added avatar content

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,27 @@
 import './index.css';
+import image from '../../assets/avatar.png';
 
 function Hero() {
   return (
-    <div className='hero-holder'>
-      <div className='hero-title'>
-        {/* <h1>Start where you are. Use what you have. Do what you can.</h1> */}
-        <h1>Start where you are.</h1>
-        <h1>Use what you have.</h1>
-        <h1>Do what you can.</h1>
+    <div>
+      <div className='hero-holder'>
+        <div className='hero-title'>
+          <h1>Start where you are.</h1>
+          <h1>Use what you have.</h1>
+          <h1>Do what you can.</h1>
+        </div>
+      </div>
+
+      <div className='main-style avatar-content'>
+        <img src={image} alt='avatar' />
+        <div>
+          <h2>👋🏽 Hello! My name is Kim.</h2>
+          <h3>
+            I am endlessly curious about everything that intersects between{' '}
+            <strong>people</strong>, <strong>design</strong>, and{' '}
+            <strong>technology</strong>
+          </h3>
+        </div>
       </div>
     </div>
   );
