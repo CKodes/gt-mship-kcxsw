ded34145a7136348c1a4a930c92e8e03629be1a2
KCas <78678795+CKodes@users.noreply.github.com>
2022-08-22 04:45:41 +0800

    style: added hero message

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,16 @@
 import './index.css';
 
 function Hero() {
-  return <div className='hero-holder'></div>;
+  return (
+    <div className='hero-holder'>
+      <div className='hero-title'>
+        {/* <h1>Start where you are. Use what you have. Do what you can.</h1> */}
+        <h1>Start where you are.</h1>
+        <h1>Use what you have.</h1>
+        <h1>Do what you can.</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
