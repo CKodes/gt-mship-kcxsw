fafe1442d5a11ac3b4390972fbf31fcc8b36e395
KCas <78678795+CKodes@users.noreply.github.com>
2022-09-26 23:54:03 +0800

    style: add classes and inline css
    replaced ICON with FaBars

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,12 +1,25 @@
 import './navbar.css';
 import logoBlack from '../../assets/LogoBlack.svg';
+import { FaBars } from 'react-icons/fa';
 
 function Navbar() {
   return (
     <nav>
-      <img className='logo-img' src={logoBlack} alt='logo' />
-
-      <div>ICON</div>
+      <div
+        className='main-style align-center'
+        style={{
+          paddingTop: '0.5rem',
+          paddingBottom: '0.5rem',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div className='align-center' style={{ paddingLeft: '1.5rem' }}>
+          <img src={logoBlack} alt='logo' />
+        </div>
+        <div className='align-center' style={{ paddingRight: '1.5rem' }}>
+          <FaBars />
+        </div>
+      </div>
     </nav>
   );
 }
