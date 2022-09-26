df1044767b576a10379983d4a80eb6bb8ca977a3
KCas <78678795+CKodes@users.noreply.github.com>
2022-09-26 22:56:08 +0800

    feat: import and add logoBlack, ICON placeholder

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,14 @@
 import './navbar.css';
+import logoBlack from '../../assets/LogoBlack.svg';
 
 function Navbar() {
-  return <nav>Hello world.</nav>;
+  return (
+    <nav>
+      <img className='logo-img' src={logoBlack} alt='logo' />
+
+      <div>ICON</div>
+    </nav>
+  );
 }
 
 export default Navbar;
