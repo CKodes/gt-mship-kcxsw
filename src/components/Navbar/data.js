5041789c353f02d1df3151656c272a11dee3f52b
KCas <78678795+CKodes@users.noreply.github.com>
2022-10-02 17:00:04 +0800

    document: added new data.js file

---
+++ src/components/Navbar/data.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    url: 'https://www.google.com',
+    text: 'home',
+  },
+  {
+    id: 2,
+    url: 'https://www.google.com',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: 'https://www.google.com',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: 'https://www.google.com',
+    text: 'contact',
+  },
+];
